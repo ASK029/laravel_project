6cac2feaa077fb9a7967c0c71931a21de0e6b526
AnasKH2001 <khazalanas909@gmail.com>
2023-12-29 20:03:44 +0300

    minor change

--- app/Http/Controllers/CatController.php
+++ app/Http/Controllers/CatController.php
@@ -23,8 +23,8 @@ class CatController extends Controller
         $category = $request->category;
         
         // Get all medicines from the specified category
-        $meds = Med::where('category', $category)->select('scientific_name','commercial_name','category','manufacturer_name','price')->get();
-        return response()->json(['test'=>$meds]);
+        $meds = Med::where('category', $category)->get();
+        return response()->json(['meds'=>$meds]);
     }
 
 }
